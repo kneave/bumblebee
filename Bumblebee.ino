5c3464acbb2d44626ea2821dcfadc07771581a59
Keegan Neave <keegan.neave@outlook.com>
2016-05-04 22:12:32 +0000

    initial chicken

---
+++ Bumblebee.ino
@@ -0,0 +1,16 @@
+#include <Adafruit_MotorShield.h>
+
+void setup()
+{
+	//  Need to setup serial
+	//  initialise things for the motor shield
+	//  do whatever else needs doing
+}
+
+void loop()
+{
+	// listen for commands on the serial port
+	// if command received, action it
+	// if no command for a while, milliseconds, then stop all actions
+	// some are bool, brush on/off for example, others momentary like motor control
+}
