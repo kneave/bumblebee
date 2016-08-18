04463ae126ef05360fdad092eca2eb1a45cec767
Keegan Neave <keegan.neave@outlook.com>
2016-08-18 12:59:21 +0100

    Hacking to get the analog stick giving the right results.

--- console_app/Program.cs
+++ console_app/Program.cs
@@ -24,19 +24,19 @@ namespace console_app
 
         static void Main(string[] args)
         {
-            _serialPort = new SerialPort();
-            _serialPort.PortName = Properties.Settings.Default.COMPort;
-            _serialPort.BaudRate = 115200;
-            _serialPort.Open();
+            //_serialPort = new SerialPort();
+            //_serialPort.PortName = Properties.Settings.Default.COMPort;
+            //_serialPort.BaudRate = 115200;
+            //_serialPort.Open();
 
-            if (_serialPort.IsOpen)
-            {
-                Console.WriteLine("Serial connected");
-            }
-            else
-            {
-                Console.WriteLine("No serial connection");
-            }
+            //if (_serialPort.IsOpen)
+            //{
+            //    Console.WriteLine("Serial connected");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No serial connection");
+            //}
 
             _controller = new Controller(UserIndex.One);
             
@@ -62,7 +62,23 @@ namespace console_app
             GetControllerInfoStrings();
             //Console.WriteLine("{0}, {1}, {2}", _leftAxis, _rightAxis, _buttons);
 
-            int leftMotorSpeed, rightMotorSpeed;
+            //int leftMotorSpeed, rightMotorSpeed;
+            //string controlString;
+            //DPadControl(out leftMotorSpeed, out rightMotorSpeed, out controlString);
+
+            LeftAnalogControl();
+
+            //if (leftMotorSpeed != 0 | rightMotorSpeed != 0)
+            //{
+            //    if (_serialPort.IsOpen)
+            //    {
+            //        _serialPort.WriteLine(controlString);
+            //    }
+            //}
+        }
+
+        private static void DPadControl(out int leftMotorSpeed, out int rightMotorSpeed, out string controlString)
+        {
             leftMotorSpeed = rightMotorSpeed = 0;
 
             switch (_controllerState.Gamepad.Buttons)
@@ -86,22 +102,13 @@ namespace console_app
                     break;
             }
 
-            string controlString = string.Format("L{0}{1:000}R{2}{3:000}W{4}B{4}",
-                leftMotorSpeed >= 0 ? "+" : string.Empty, 
+            controlString = string.Format("L{0}{1:000}R{2}{3:000}W{4}B{4}",
+                leftMotorSpeed >= 0 ? "+" : string.Empty,
                 leftMotorSpeed,
-                rightMotorSpeed >= 0 ? "+" : string.Empty, 
+                rightMotorSpeed >= 0 ? "+" : string.Empty,
                 rightMotorSpeed,
                 cleaning ? "+255" : "000");
-
             Console.WriteLine(controlString);
-
-            if (leftMotorSpeed != 0 | rightMotorSpeed != 0)
-            {
-                if (_serialPort.IsOpen)
-                {
-                    _serialPort.WriteLine(controlString);
-                }
-            }
         }
 
         private static void LeftAnalogControl()
@@ -110,11 +117,7 @@ namespace console_app
             int yValue = _controllerState.Gamepad.LeftThumbY;
             Tuple<double, double> motorPower = MotorValues(xValue, yValue);
 
-            Console.WriteLine("Controller: {0},{1}; Power: {2},{3}",
-                xValue,
-                yValue,
-                motorPower.Item1,
-                motorPower.Item2);
+            
         }
 
         private static void GetControllerInfoStrings()
@@ -137,55 +140,71 @@ namespace console_app
 
             bool xValid = x >= deadzoneX & x <= (deadzoneX * -1) ? true : false;
             bool yValid = y >= deadzoneY & y <= (deadzoneY * -1) ? true : false;
-                       
+
+            double clampedX = (((x + 32768) / (32768 * 2)) * 20) - 10;
+            double clampedY = (((y + 32768) / (32768 * 2)) * 20) - 10;
+                
 
             //if (xValid | yValid) // or dead zone values
             if (true)
             {
                 // what will be how fast we move, based on how far we are from the centre point.
-                double power = Math.Sqrt(x * x + y * y);
+                double power = Math.Sqrt(clampedX * clampedX + clampedY * clampedY);
                 // figure out angle from y axis of x,y cord
                 double angle = 0D;
-                int xS = Math.Sign(x);
-                int yS = Math.Sign(y);
-                if (x == 0 && yS > 0)
+                int xS = Math.Sign(clampedX);
+                int yS = Math.Sign(clampedY);
+                if (clampedX == 0 && yS > 0)
                 {
                     angle = 0;
                 }
-                else if (y == 0 && xS > 0)
+                else if (clampedY == 0 && xS > 0)
                 {
                     angle = (Math.PI / 2);
                 }
-                else if (x == 0 && yS < 0)
+                else if (clampedX == 0 && yS < 0)
                 {
                     angle = Math.PI;
                 }
-                else if (y == 0 && xS < 0)
+                else if (clampedY == 0 && xS < 0)
                 {
                     angle = (Math.PI + (Math.PI / 2));
                 }
                 else if (xS > 0 && yS > 0)
                 {
-                    angle = Math.Tan(y / x);
+                    angle = Math.Tan(clampedY / clampedX);
                 }
                 else if (xS < 0 && yS > 0)
                 {
-                    angle = (Math.PI / 2) + Math.Tan(x / y);
+                    angle = (Math.PI / 2) + Math.Tan(clampedX / clampedY);
                 }
                 else if (xS < 0 && yS < 0)
                 {
-                    angle = Math.PI + Math.Tan(x / y);
+                    angle = Math.PI + Math.Tan(clampedX / clampedY);
                 }
                 else
                 {
-                    angle = (Math.PI + (Math.PI / 2)) + Math.Tan(y / x);
+                    angle = (Math.PI + (Math.PI / 2)) + Math.Tan(clampedY / clampedX);
                 }
                 double offset = Math.PI - ((3 * Math.PI) / 4);  // the point the cos and sin curves cross over.
                 motorPowerLeft = Math.Sin(angle + offset) * power;
                 motorPowerRight = Math.Cos(angle + offset) * power;
+
+                Console.WriteLine("Controller: {0},{1}; clamped: {2},{3} angle {4} p{5} Power: {6},{7}",
+                x,
+                y,
+                clampedX,
+                clampedY,
+                (angle / Math.PI) * 180,
+                power,
+                motorPowerLeft,
+                motorPowerRight
+                );
             }
 
             Tuple<double, double> motorPower = new Tuple<double, double>(motorPowerLeft, motorPowerRight);
+
+            
             return motorPower;
         }
     }
